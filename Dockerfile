8dd5b3a21570f97b36a9e092f84e2ed5c792b1d8
jinbo <jinbo@mgtv.com>
2023-04-11 20:22:20 +0800

    fix: update base image

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM ghcr.io/zhayujie/chatgpt-on-wechat:latest
+FROM kinboy/chatgpt-on-wechat:latest
 
 ENTRYPOINT ["/entrypoint.sh"]
--- docker/build.alpine.sh
+++ docker/build.alpine.sh
@@ -5,12 +5,15 @@ CHATGPT_ON_WECHAT_TAG=`curl -sL "https://api.github.com/repos/zhayujie/chatgpt-o
      grep '"tag_name":' | \
      sed -E 's/.*"([^"]+)".*/\1/'`
 
+HTTP_PROXY=http://192.168.192.128:7890
+
 # build image
 docker build -f Dockerfile.alpine \
+             --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTPS_PROXY \
              --build-arg CHATGPT_ON_WECHAT_VER=$CHATGPT_ON_WECHAT_TAG \
-             -t zhayujie/chatgpt-on-wechat .
+             -t kinboy/chatgpt-on-wechat .
 
 # tag image
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:alpine 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$CHATGPT_ON_WECHAT_TAG-alpine
+docker tag kinboy/chatgpt-on-wechat kinboy/chatgpt-on-wechat:alpine 
+docker tag kinboy/chatgpt-on-wechat kinboy/chatgpt-on-wechat:$CHATGPT_ON_WECHAT_TAG-alpine
            
